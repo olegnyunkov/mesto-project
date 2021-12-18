d1fae48baf95e8ea19b4a89e00a8047339cf170a
Oleg Nyunkov <nvo84@yandex.ru>
2021-12-18 23:59:18 +0600

    feat: added card modal window

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -1,3 +1,5 @@
+
+// открытие и закрытие формы профиля
 const popup = document.querySelector('.popup');
 const formClose = popup.querySelector('.form__close');
 const profileEdit = document.querySelector('.profile__edit');
@@ -15,7 +17,7 @@ function popupClosed() {
 profileEdit.addEventListener('click', popupOpened);
 formClose.addEventListener('click', popupClosed);
 
-
+// редактирование и сохренение данных профиля
 const formElement = document.querySelector('.form__input');
 const nameInput = document.querySelector('.form__field-name');
 const jobInput = document.querySelector('.form__field-about');
@@ -26,7 +28,56 @@ const jobInput = document.querySelector('.form__field-about');
     let jobNameInput = jobInput.value;
     document.querySelector('.profile__name').textContent = formNameInput;
     document.querySelector('.profile__text').textContent = jobNameInput;
+    popupClosed();
   }
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+// массив данных для карточек
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// открытие и закрытие формы карточек
+const popupPlace = document.querySelector('.popup-place');
+const formPlaceClose = popupPlace.querySelector('.form-place__close');
+const placeEdit = document.querySelector('.profile__add');
+
+function popupPlaceOpened() {
+  popupPlace.classList.add('popup-place_opened');
+  popupPlace.classList.remove('popup-place_closed');
+}
+
+function popupPlaceClosed() {
+  popupPlace.classList.add('popup-place_closed');
+  popupPlace.classList.remove('popup-place_opened')
+}
+
+placeEdit.addEventListener('click', popupPlaceOpened);
+formPlaceClose.addEventListener('click', popupPlaceClosed);
+
+// добавление и сохранение карточек
 
