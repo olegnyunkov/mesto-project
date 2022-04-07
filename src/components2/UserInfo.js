e2041ae50b82f4e0486606b2934b0ebe8a2ec57d
Zhenia <zhenia1985@gmail.com>
2022-04-08 05:18:29 +0700

    merg

--- src/components2/UserInfo.js
+++ src/components2/UserInfo.js
@@ -1,4 +1,21 @@
 export class UserInfo {
+   constructor(name, about, avatar) {
+       this._name = name;
+       this._about = about;
+       this._avatar = avatar;
+   }
+
+   setUserInfo() {
+       const profileName = document.querySelector('.profile__name');
+       const profileDescription = document.querySelector('.profile__text');
+       const avatarPhoto = document.querySelector('.profile__avatar');
+       profileName.textContent = this._name;
+       profileDescription.textContent = this._about;
+       avatarPhoto.src = this._avatar;
+   }
+}
+
+export class UserInfo {
    constructor(profileNameInput, profileDescriptionInput, avatarPhoto, ava) {
 
       // this._name = document.querySelector(profileNameInput)
---
+++ src/utils/constants.js
@@ -0,0 +1,16 @@
+export const apiConfig = {
+   url: 'https://nomoreparties.co/v1/plus-cohort7/',
+   headers: {
+      authorization: '55f6dcbe-e189-42c3-b858-3cc6208e5fc5',
+      'Content-Type': 'application/json'
+   },
+}
+
+export const validationConfig = {
+   formSelector: '.form',
+   inputSelector: '.form__input',
+   buttonSelector: '.form__save',
+   inputErrorClass: 'form__input_error',
+   textErrorClass: 'form__error_visible',
+   buttonErrorClass: 'form__save_disabled',
+ };
